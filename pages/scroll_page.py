17577db8c97ab96e4a2142c9d572a0f38bd4fbd9
Ruslan <apanaevrus@gmail.com>
2026-09-02 23:10:41 +0400

    02.09.26 Home footer

--- pages/scroll_page.py
+++ pages/scroll_page.py
@@ -9,12 +9,15 @@ class ScrollPage(PageActions):
         super().__init__(page)
         self.scroll_locators = MultiWebElement(page.locator("//div[contains(@class, 'jscroll') "
                                                             "and contains(@class, 'added')]"), "Абзац")
+        self.footer = page.locator("#page-footer")
 
     def scroll_down(self):
         next_paragraph = self.scroll_locators.nth(self.count_paragraph())
-        last_paragraph = self.scroll_locators.nth(-1)
-        last_paragraph.scroll_into_view_if_needed()
-        next_paragraph.wait_for_attached_state()
+        self.footer.scroll_into_view_if_needed()
+        self.footer.wait_for(state="visible")
+        print("1")
+
+        next_paragraph.locator.wait_for(state="visible")
 
     def count_paragraph(self):
         return self.scroll_locators.count()
