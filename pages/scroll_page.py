752ac24d960d7e09879a4d5cc9d8aac3f96d866c
Руслан Апанаев <apanaevrus@gmail.com>
2026-09-04 10:46:45 +0400

    04.09.26

--- pages/scroll_page.py
+++ pages/scroll_page.py
@@ -7,16 +7,18 @@ from ui.page_actions import PageActions
 class ScrollPage(PageActions):
     def __init__(self, page: Page):
         super().__init__(page)
-        self.scroll_locators = MultiWebElement(page.locator("//div[contains(@class, 'jscroll')]"), "Абзац")
-        self.footer = page.locator("#page-footer")
+        self.scroll_locators = MultiWebElement(
+            page.locator("//div[contains(@class, 'jscroll') and contains(@class, 'added') "
+                         "and normalize-space(text()) != '']"), "Абзац")
 
     def scroll_down(self):
-        count_para = self.count_paragraph()
-        future_paragraph = self.scroll_locators.nth(self.count_paragraph())
-        while count_para == self.count_paragraph():
-            self.footer.scroll_into_view_if_needed()
-        else:
-            future_paragraph.wait_for_attached_state()
+        count = self.count_paragraph()
+        next_paragraph = self.scroll_locators.nth(count)
+
+        last_paragraph = self.scroll_locators.nth(-1)
+        last_paragraph.scroll_into_view_if_needed()
+
+        next_paragraph.wait_for_attached_state()
 
     def count_paragraph(self):
         return self.scroll_locators.count()
